b1ce6403137d88e3dfbe04c60094f59e05c1299a
Xinjie <56352426@qq.com>
2022-03-24 21:55:39 +0800

    third commit

--- src/main/java/com/atguigu/git/GitText.java
+++ src/main/java/com/atguigu/git/GitText.java
@@ -4,6 +4,7 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello gitttttt");
+        System.out.println("hello gitttttt");
     }
 
 }

0d70a291af768dc0d886b0bd1f4736f8d9b39a61
Xinjie <56352426@qq.com>
2022-03-24 21:37:10 +0800

    second commir

--- src/main/java/com/atguigu/git/GitText.java
+++ src/main/java/com/atguigu/git/GitText.java
@@ -3,6 +3,7 @@ package com.atguigu.git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello gitttttt");
     }
 
 }
